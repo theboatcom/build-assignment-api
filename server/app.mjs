eb149fe098677fde9e353fd48d481468d829e826
theboatcom <tanakonintapat@gmail.com>
2024-07-16 11:09:31 +0700

    assignment-be2-boat

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,27 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
-app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
+app.post("/assignments", async (req, res) => {
+ 
+  let results 
+  try {
+   results = await connectionPool.query(`select * from assignments`);
+  }
+  catch{
+    return res.status(500).json({
+       message: "Server could not read assignment because database connection" 
+    })
+  }
+  
+  return res.status(201).json({
+    data: results.rows,
+  });
+  
+
+ //return res.st("Server API is working 🚀");
 });
 
 app.listen(port, () => {
